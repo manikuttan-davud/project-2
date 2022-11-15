8d0deb3afac990b03328d42da7cf530c8a168725
Vignesh <vignesh@stunntech.com>
2022-11-15 20:55:21 +0530

    Converted the data to models

--- lib/utilts/data.dart
+++ lib/utilts/data.dart
@@ -1,22 +1,20 @@
-List recommendedSongs =[
+List recommendedSongs = [
   {
     'name': 'Born To Die',
-    'artist':'Lena Del Rey',
-    'url':'https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/316400/316486.png'
+    'artist': 'Lena Del Rey',
+    'url':
+        'https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/316400/316486.png'
   },
-   {
+  {
     'name': 'Born To Die2',
-    'artist':'Lena Del Rey2',
-    'url':'https://images.hindustantimes.com/img/2022/09/02/550x309/tendulkar_mi_ipl_1650777563845_1662102044860_1662102044860.webp'
+    'artist': 'Lena Del Rey2',
+    'url':
+        'https://images.hindustantimes.com/img/2022/09/02/550x309/tendulkar_mi_ipl_1650777563845_1662102044860_1662102044860.webp'
   },
-   {
+  {
     'name': 'Born To Die3',
-    'artist':'Lena Del Rey3',
-    'url':'https://upload.wikimedia.org/wikipedia/commons/2/25/Sachin_Tendulkar_at_MRF_Promotion_Event.jpg'
+    'artist': 'Lena Del Rey3',
+    'url':
+        'https://upload.wikimedia.org/wikipedia/commons/2/25/Sachin_Tendulkar_at_MRF_Promotion_Event.jpg'
   },
-   {
-    'name': 'Born To Die4',
-    'artist':'Lena Del Rey4',
-    'url':'https://img.etimg.com/photo/msid-82701259,quality-100/talking-about-mental-health-which-has-gained-significance-in-the-covid-times-with-players-spending-lot-of-time-in-bio-bubbles-tendulkar-said-acceptance-is-the-key-.jpg'
-  }
 ];
